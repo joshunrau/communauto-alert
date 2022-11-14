0e932c4e1fe0a52ee37166ca28331c400b7af121
joshunrau <contact@joshuaunrau.com>
2022-11-14 00:56:53 -0500

    add args

--- communauto_alert/main.py
+++ communauto_alert/main.py
@@ -1,2 +1,11 @@
-def main():
-    print('alert')
+import argparse
+
+from importlib.metadata import version
+
+def main() -> None:
+    parser = argparse.ArgumentParser(prog='communauto-alert', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s {version('communauto-alert')}")
+    parser.add_argument('latitude', help='your latitude', type=float)
+    parser.add_argument('longitude', help='your longitude', type=float)
+    parser.add_argument('--max_distance', help='max distance of vehicle in meters', default=500, metavar='', type=int)
+    args = parser.parse_args()
