390cc6fcc2a2c611aa901ebe3337e98d56719cb6
joshunrau <contact@joshuaunrau.com>
2022-11-14 01:31:07 -0500

    works

--- communauto_alert/main.py
+++ communauto_alert/main.py
@@ -1,6 +1,9 @@
 import argparse
+import time
 
 from importlib.metadata import version
+
+from .vehicles import Vehicles
 
 def main() -> None:
     parser = argparse.ArgumentParser(prog='communauto-alert', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -9,3 +12,12 @@ def main() -> None:
     parser.add_argument('longitude', help='your longitude', type=float)
     parser.add_argument('--max_distance', help='max distance of vehicle in meters', default=500, metavar='', type=int)
     args = parser.parse_args()
+
+    vehicles = Vehicles()
+    while True:
+        vehicles.fetch()
+        closest_vehicle, distance = vehicles.get_closest(args.latitude, args.longitude)
+        if args.max_distance > distance:
+            print(closest_vehicle, distance)
+            return None
+        time.sleep(60)
